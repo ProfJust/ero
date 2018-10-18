8be9921c804de315ad3036d516bacfd4d19fa930
Prof. Just <olaf.just@w-hs.de>
2018-10-18 11:35:42 +0200

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/oj/catkin_ws/src/ARO_summit
-# Build directory: /home/oj/catkin_ws/build/ARO_summit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
