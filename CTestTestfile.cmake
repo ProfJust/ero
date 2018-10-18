4a2907b26af5cda35fe7fd237666e06145b174d2
Prof. Just <olaf.just@w-hs.de>
2018-10-18 11:20:00 +0200

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/oj/catkin_ws/src/ARO_summit
+# Build directory: /home/oj/catkin_ws/build/ARO_summit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
