6d4b6b0d301ffa0ac67eb04eef97c701012e6773
Prof. Just <olaf.just@w-hs.de>
2020-04-30 17:28:56 +0200

    Delete HelloWorld.py

--- ero_python_ws/ero_V1_sw/HelloWorld.py
+++
@@ -1,9 +0,0 @@
-# HelloWorld.py	
-#-----------------------------------
-# 14.09.2018 by OJ
-#----------------------------------- 
-def main():    
-    print("Hello Python World")
-    myFirstString = "Hallo liebe Studenten"
-    print(myFirstString)
-main()
