ebd2f9ced5a5b44cbe827b5bdade0c60a2eedea4
ProfJust <olaf.just@w-hs.de>
2021-04-15 16:07:45 +0200

    SS21

--- bin/ero_p1_bmi/BMI.py
+++ bin/ero_p1_bmi/BMI.py
@@ -5,30 +5,30 @@
 # -----------------------------------
 
 
-def bmi(m, l):
-    bmi = m/(l*l)
+def bmi(kg, m):
+    bmi = kg/(m*m)
     return bmi
 
 
 def bewertung(b):
     if (b > 25):
-		print(" => Uebergewicht")
-	elif (b < 18.5):
-		print(" => Untergewicht")
-	else:	
-		print(" => Normalgewicht")
+        print(" => Uebergewicht")
+    elif (b < 18.5):
+        print(" => Untergewicht")
+    else:
+        print(" => Normalgewicht")
 
 
-def main():    
-	gewicht = input("Geben Sie Ihr Gewicht in kg an \n")
-	laenge  = input("Geben Sie Ihre Koerpergroesse in m an \n")
-	#alter   = input("Geben Sie Ihr Alter in Jahren an \n");
+def main():
+    gewicht = input("Geben Sie Ihr Gewicht in kg an \n")
+    laenge = input("Geben Sie Ihre Koerpergroesse in m an \n")
+    # alter   = input("Geben Sie Ihr Alter in Jahren an \n");
 
-	_bmi = bmi(gewicht, laenge)
-	
-	print("BMI: ",_bmi)
-	bewertung(_bmi)
-		
+    _bmi = bmi(gewicht, laenge)
+
+    print("BMI: ", _bmi)
+    bewertung(_bmi)
+
 
 # ------------------------------------------------------
 main()
