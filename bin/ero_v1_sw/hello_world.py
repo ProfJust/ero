8b10fd7edf1c9fe6be7fe86ed2fc8a55d8980860
ProfJust <olaf.just@w-hs.de>
2021-04-15 15:11:30 +0200

    SS21

--- bin/ero_v1_sw/hello_world.py
+++ bin/ero_v1_sw/hello_world.py
@@ -3,7 +3,8 @@
 # -------------------------------
 
 def say_hello():
-	print(" Hello Python World")
-	
+    print(" Hello Python World")
+
+
 if __name__ == '__main__':
-	say_hello() 
+    say_hello()
