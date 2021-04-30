2e72b5bbb259f99aaaecacf549bc611e9019bbc6
ProfJust <olaf.just@w-hs.de>
2021-04-30 15:39:09 +0200

    ss21

--- nodes/ero_p5_gazebo_patrol/whs_gazebo_patrol.py
+++ nodes/ero_p5_gazebo_patrol/whs_gazebo_patrol.py
@@ -19,11 +19,10 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 # Tipp: mit rostopic echo /clicked_point werden die in RVIZ
 # angeklickten Punkte (Publish Point) angezeigt
 waypoints = [
-    [(-3.39, 1.95, 0.000), (0.000, 0.000, -0.990, 0.114)],  # per clicked point
-    [(4.547, -2.71, 0.000), (0.000, 0.000, -0.775, 0.655)],
-    [(-3.757, -1.03, 0.000), (0.000, 0.000, 0.010, 0.999)],
+    [(-3.3, 2.0, 0.000), (0.000, 0.000, 0.990, 0.914)],  # per clicked point
+    [(-3.3, 10.0, 0.000), (0.000, 0.000, 0.990, 0.914)],
+    [(-3.0, 27.0, 0.000), (0.000, 0.000, 1.0, 1.0)],
 ]
-
 
 # Helper Function to turn waypoint => MoveBaseGoal
 def set_goal_pose(pose):
@@ -57,7 +56,7 @@ if __name__ == '__main__':
     while not rospy.is_shutdown():
         for pose in waypoints:
             next_goal_pose = set_goal_pose(pose)
-            print('Robot should now go to waypoint '+str(wayPointNr)+' ')
+            print('Robot should now go to waypoint ' + str(wayPointNr) + ' ')
             print(str(next_goal_pose.target_pose.pose))
             wayPointNr = wayPointNr+1
             client.send_goal(next_goal_pose)
